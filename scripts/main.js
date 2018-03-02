0decefad27f007926ce7dd14d87432f3b62c5455
Elis Jones <ej14433@my.bristol.ac.uk>
2018-03-02 17:25:00 +0000

    Some change

--- scripts/main.js
+++ scripts/main.js
@@ -3,21 +3,21 @@
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  //console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
   console.log('Full Name: ' + profile.getName());
-  //console.log('Given Name: ' + profile.getGivenName());
-  //console.log('Family Name: ' + profile.getFamilyName());
-  //console.log("Image URL: " + profile.getImageUrl());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
 
   // The ID token you need to pass to your backend:
-  //var id_token = googleUser.getAuthResponse().id_token;
-  //console.log("ID Token: " + id_token);
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
 
   console.log("Email: " + profile.getEmail());
-  window.location.href = "http://ej14433.github.io/test.html";
+  // window.location.href = "http://ej14433.github.io/test.html";
 
-  var email = profile.getEmail();
-  var body = document.getElementsByClassName('body');
-  body.innerHTML += email;
-  body.innerHTML += 'email';
+  //var email = profile.getEmail();
+  //var body = document.getElementsByClassName('body');
+  //body.innerHTML += email;
+  //body.innerHTML += 'email';
 };
